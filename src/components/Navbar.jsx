48d0a36f2e92613fff73975fff8e7e5b01491db3
Yejun David Choi <yejunchoi21@gmail.com>
2026-09-07 16:02:44 -0400

    Create navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <header className="navbar">
+      <h1 className="navbar-logo">Bakeryhouse</h1>
+
+      <nav className="navbar-links">
+        <a href="/">Home</a>
+        <a href="/menu">Menu</a>
+        <a href="/about">About</a>
+        <a href="/cart">Cart</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
