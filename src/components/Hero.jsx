b99e330a0c8ff7e67e19ff82c5772f6affc80ece
Yejun David Choi <yejunchoi21@gmail.com>
2026-09-08 11:08:50 -0400

    Added hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import "./Hero.css";
+import breadImage from "../assets/images/image1.png"
+function Hero(){
+    return(
+
+
+    <main className="hero">
+        <div className="hero-content">
+            <p>Fresh goods</p>
+            <h2>Handcrafted treats made with care</h2>
+
+            <a href="/menu">View our Menu</a>
+        </div>
+
+        <div className="hero-image">
+        <img src={breadImage} alt="Freshly baked bread" />
+      </div>
+    </main>
+    );
+}
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,9 @@ function Navbar() {
   return (
     <header className="navbar">
       <h1 className="navbar-logo">Bakeryhouse</h1>
-
       <nav className="navbar-links">
         <a href="/">Home</a>
-        <a href="/menu">Menu</a>
+        <a href="/menu">Menu </a>
         <a href="/about">About</a>
         <a href="/cart">Cart</a>
       </nav>
